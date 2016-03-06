4c5819a2313bafffe6f36be190ee4db4c3b25524
LEOnardo Ma <leo_ma@sfu.ca>
2016-03-05 16:37:01 -0800

    simple database

--- app/src/main/java/com/example/leo/momentcontact/LandingActivity.java
+++ app/src/main/java/com/example/leo/momentcontact/LandingActivity.java
@@ -1,13 +1,40 @@
 package com.example.leo.momentcontact;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class LandingActivity extends AppCompatActivity {
+
+    public MyDatabase myDatabase;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_landing);
+
+        myDatabase = new MyDatabase(this);
+
+    }
+
+    public void addSomething(View view){
+        long id = myDatabase.insertData("LEO", "123", "01001", "1241233");
+        if (id < 0)
+        {
+            Toast.makeText(this, "fail", Toast.LENGTH_SHORT).show();
+        }
+        else
+        {
+            Toast.makeText(this, "success", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    public void viewResults(View view)
+    {
+
+        Intent intent = new Intent(this, AllRowActivity.class);
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/com/example/leo/momentcontact/MyDatabase.java
@@ -0,0 +1,52 @@
+package com.example.leo.momentcontact;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.widget.Toast;
+
+/**
+ * Created by LEO on 2016-03-05.
+ */
+public class MyDatabase {
+
+    private Context context;
+    private final MySQLiteHelper mySQLiteHelper;
+    private SQLiteDatabase sqLiteDatabase;
+
+    public MyDatabase (Context context) {
+        this.context = context;
+        mySQLiteHelper = new MySQLiteHelper(context);
+
+        sqLiteDatabase = mySQLiteHelper.getWritableDatabase();
+
+    }
+
+    public long insertData (String name, String password
+            , String stanleypark, String policestation
+    )
+    {
+
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(Constants.NAME, name);
+        contentValues.put(Constants.PASSWORD, password);
+        contentValues.put(Constants.STANLEY_PARK, stanleypark);
+        contentValues.put(Constants.POLICE_STATION, policestation);
+        long id = sqLiteDatabase.insert(Constants.TABLE_NAME, null, contentValues);
+
+        return id;
+    }
+
+
+    public Cursor getData()
+    {
+
+        String[] columns = {Constants.UID, Constants.NAME, Constants.PASSWORD
+                , Constants.STANLEY_PARK, Constants.POLICE_STATION
+        };
+        Cursor cursor = sqLiteDatabase.query(Constants.TABLE_NAME, columns, null, null, null, null, null);
+        return cursor;
+    }
+
+}
